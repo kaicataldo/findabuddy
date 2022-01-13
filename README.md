710f1d234ed444b3178cae12bc7f848a5bdbc0e4
Kai Cataldo <kai@kaicataldo.com>
2022-01-13 12:42:31 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,74 @@
+Find-A-Buddy
+==========
+
+      ,:'/  _ ..._         ______ _           _                    ____            _     _
+     // ( `""-.._.'       |  ____(_)         | |        /\        |  _ \          | |   | |
+     \| /    6\___        | |__   _ _ __   __| |______ /  \ ______| |_) |_   _  __| | __| |_   _
+     |     6      4       |  __| | | '_ \ / _` |______/ /\ \______|  _ <| | | |/ _` |/ _` | | | |
+     |            /       | |    | | | | | (_| |     / ____ \     | |_) | |_| | (_| | (_| | |_| |
+     \_       .--'        |_|    |_|_| |_|\__,_|    /_/    \_\    |____/ \__,_|\__,_|\__,_|\__, |
+     (_'---'`)                                                                              __/ |
+                                                                                           |___/
+
+
+Find-A-Buddy is a Twitter bot that uses the PetFinder API to tweet listings of adoptable dogs in a given geographic area. To create your own Find-A-Buddy Twitter bot, all you need is your own [Petfinder API key](https://www.petfinder.com/developers/api-key), your own [Twitter app auth credentials](https://apps.twitter.com/), and a process to invoke the script at a set interval.
+
+This is a rewrite of one of my very first [projects](https://github.com/kaicataldo/findabuddy-v1), this time written in Python since it's the current language I'm learning 😊.
+
+# Usage
+
+This little script was written with the intention of making it easy for others to stand up their own local Find-A-Buddy Twitter account. The author is new to Python at the time this was written, so any input on improving this setup would be greatly appreciated!
+
+You'll need to set up some sort of cron job that runs the script at a regular interval. Do be careful with this - Twitter has guidelines about what they think consitutes spam. Learn more about these guidelines [here](https://dev.twitter.com/overview/terms/policy).
+
+## Running the script
+
+1. Install [pipenv](https://pipenv.pypa.io/en/latest/).
+1. Clone the repo into the execution environment.
+1. Run `pipenv install` in the root directory of this repository.
+1. Run `pipenv run python findabuddy` with the required options outlined below.
+
+## Configuration
+
+### Options
+
+Find-A-Buddy accepts the following command line arguments:
+
+```sh
+  -h, --help            show this help message and exit
+  --petfinder_api_key PETFINDER_API_KEY
+                        Your key for the Petfinder API
+  --petfinder_secret PETFINDER_SECRET
+                        Your secret for the Petfinder API
+  --twitter_api_key TWITTER_API_KEY
+                        Your key for the Twitter v2 API
+  --twitter_api_secret TWITTER_API_SECRET
+                        Your secret for the Twitter v2 API
+  --twitter_access_token TWITTER_ACCESS_TOKEN
+                        Your access token for your Twitter v2 developer account
+  --twitter_access_token_secret TWITTER_ACCESS_TOKEN_SECRET
+                        Your access token secret for your Twitter v2 developer account
+  --location LOCATION   The location you would like to search listings for. Formats: city, state; latitude,longitude; or postal code
+  --distance DISTANCE   (Optional) Search for results within distance of location (in miles). Defaults to 25.
+  --twitter_account_name TWITTER_ACCOUNT_NAME
+                        (Optional) Twitter account name for the generated tweet. Used to construct a URL
+```
+
+### Example
+
+```sh
+pipenv run python findabuddy \
+    --petfinder_api_key <Petfinder API key> \
+    --petfinder_secret <Petfinder API secret> \
+    --twitter_api_key <Twitter API key> \
+    --twitter_api_secret <Twitter API secret> \
+    --twitter_access_token <Twitter Developer account access token> \
+    --twitter_access_token_secret <Twitter Developer account access token secret> \
+    --location "New York, New York" \
+    --distance 50 \
+    --twitter_account_name "FindABuddyNYC"
+```
+
+# Dedication
+
+This project is dedicated to [Henri](https://www.instagram.com/henrisnuggles/), who reminds you to please adopt instead of buying!
---
+++ findabuddy/__main__.py
@@ -0,0 +1,263 @@
+#!/usr/bin/env python3
+
+import argparse
+import random
+import sys
+import traceback
+from textwrap import indent
+import requests
+import tweepy
+
+PETFINDER_TOKEN_URL = "https://api.petfinder.com/v2/oauth2/token"
+PETFINDER_API_URL = "https://api.petfinder.com/v2/animals"
+MAX_ATTEMPTS = 3
+
+DEFAULT_NAMES = ["pup", "pupper", "doggie", "cutie"]
+SINGLE_END_PHRASES = [
+    "needs a loving home!",
+    "is looking for a new family!",
+    "is looking for a furever home!",
+    "needs a new best friend!",
+    "needs a place to call home!",
+    "is looking for a forever home!",
+    "wants to be your buddy!",
+    "is looking for a loving family!",
+    "is in need of love!",
+    "is in need of a loving home!",
+    "is looking for a new home!",
+    "needs some lovin'!",
+    "could be your new buddy!",
+]
+MULTIPLE_END_PHRASES = [
+    "need a loving home!",
+    "are looking for a new family!",
+    "are looking for a furever home!",
+    "need a new best friend!",
+    "need a place to call home!",
+    "are looking for a forever home!",
+    "want to be your buddy!",
+    "are looking for a loving family!",
+    "are in need of love!",
+    "are in need of a loving home!",
+    "are looking for a new home!",
+    "need some lovin'!",
+    "could be your new buddies!",
+]
+
+
+def print_indented(msg: str, indent_levels: int = 1) -> None:
+    print(indent(msg, " " * 4 * indent_levels))
+
+
+def get_cli_args() -> dict:
+    parser = argparse.ArgumentParser(
+        description="🐶 Find adoptable dogs looking for a loving home"
+    )
+
+    parser.add_argument(
+        "--petfinder_api_key",
+        help="Your key for the Petfinder API",
+        required=True,
+    )
+    parser.add_argument(
+        "--petfinder_secret",
+        help="Your secret for the Petfinder API",
+        required=True,
+    )
+    parser.add_argument(
+        "--twitter_api_key",
+        help="Your key for the Twitter v2 API",
+        required=True,
+    )
+    parser.add_argument(
+        "--twitter_api_secret",
+        help="Your secret for the Twitter v2 API",
+        required=True,
+    )
+    parser.add_argument(
+        "--twitter_access_token",
+        help="Your access token for your Twitter v2 developer account",
+        required=True,
+    )
+    parser.add_argument(
+        "--twitter_access_token_secret",
+        help="Your access token secret for your Twitter v2 developer account",
+        required=True,
+    )
+    parser.add_argument(
+        "--location",
+        help="The location you would like to search listings for. Formats: city, state; latitude,longitude; or postal code",  # noqa: E501
+        required=True,
+    )
+    parser.add_argument(
+        "--distance",
+        help="(Optional) Search for results within distance of location (in miles). Defaults to 25.",  # noqa: E501
+        type=int,
+        default=25,
+        required=False,
+    )
+    parser.add_argument(
+        "--twitter_account_name",
+        help="(Optional) Twitter account name for the generated tweet. Used to construct a URL",  # noqa: E501
+        required=False,
+    )
+
+    args = parser.parse_args()
+
+    return {
+        "petfinder_api_key": args.petfinder_api_key,
+        "petfinder_secret": args.petfinder_secret,
+        "twitter_api_key": args.twitter_api_key,
+        "twitter_api_secret": args.twitter_api_secret,
+        "twitter_access_token": args.twitter_access_token,
+        "twitter_access_token_secret": args.twitter_access_token_secret,
+        "twitter_account_name": args.twitter_account_name,
+        "location": args.location,
+        "distance": args.distance,
+    }
+
+
+def get_petfinder_oath_token(client_id: str, client_secret: str) -> str:
+    data = {
+        "grant_type": "client_credentials",
+        "client_id": client_id,
+        "client_secret": client_secret,
+    }
+    response = requests.post(
+        PETFINDER_TOKEN_URL,
+        data=data,
+    )
+    response.raise_for_status()
+    token = response.json().get("access_token")
+
+    # Just in case an "access_token" isn't provided and the API
+    # doesn't respond with the correct HTTP response code.
+    if not (token and token.strip()):
+        raise Exception("Petfinder OAuth token not included in response payload.")
+
+    return token
+
+
+def get_random_dog_listing(
+    client_id: str, client_secret: str, location: str, distance: int
+) -> dict:
+    token = get_petfinder_oath_token(client_id, client_secret)
+    headers = {"content-type": "application/json", "Authorization": f"Bearer {token}"}
+    # https://www.petfinder.com/developers/v2/docs/#get-animals
+    params = {
+        "location": location,
+        "distance": distance,
+        "status": "adoptable",
+        "type": "Dog",
+        "sort": "random",
+        "limit": 1,
+    }
+    response = requests.get(PETFINDER_API_URL, headers=headers, params=params)
+    response.raise_for_status()
+    results = response.json().get("animals")
+
+    if not results:
+        raise Exception(f"No dog listings found for {location}.")
+
+    listing = results[0]
+
+    if type(listing) is not dict:
+        raise Exception("Listing is not a JSON object.")
+
+    return listing
+
+
+def validate_name(name: str) -> str:
+    # Simple heuristic to catch some cases where the name might be updated
+    # but the status of the listing hasn't been updated yet.
+    if "adoption pending" in name.lower():
+        raise Exception("Name indicates this dog is in the process of being adopted!")
+    return name
+
+
+def is_multiple(name: str) -> bool:
+    return (
+        name is not None
+        and ("&" in name or " and " in name)
+        and not ("-" in name or "~" in name or ("(" in name and ")" in name))
+    )
+
+
+def generate_tweet(listing: dict) -> str:
+    name = validate_name(
+        listing.get("name", f"This {random.choice(DEFAULT_NAMES)}").strip()
+    )
+    end_phrase = random.choice(
+        MULTIPLE_END_PHRASES if is_multiple(name) else SINGLE_END_PHRASES
+    )
+    url = listing.get("url")
+    if not url:
+        raise Exception("No valid URL found.")
+
+    return f"{name} {end_phrase} {url}"
+
+
+def send_tweet(
+    tweet_text: str,
+    api_key: str,
+    api_secret: str,
+    access_token: str,
+    access_token_secret: str,
+) -> tweepy.Response:
+    client = tweepy.Client(
+        consumer_key=api_key,
+        consumer_secret=api_secret,
+        access_token=access_token,
+        access_token_secret=access_token_secret,
+    )
+    response = client.create_tweet(text=tweet_text)
+    # TODO: Make this exception handling more robust.
+    # This is to ensure type saftey in Pylance.
+    assert type(response) is tweepy.Response
+    return response
+
+
+def main() -> None:
+    cli_args = get_cli_args()
+    response = None
+
+    print("\n🐶 Finding a buddy!")
+    try:
+        for i in range(1, MAX_ATTEMPTS + 1):
+            print(f"🐾 Tweet attempt #{i}...")
+
+            try:
+                listing = get_random_dog_listing(
+                    cli_args["petfinder_api_key"],
+                    cli_args["petfinder_secret"],
+                    cli_args["location"],
+                    cli_args["distance"],
+                )
+                tweet_text = generate_tweet(listing)
+                response = send_tweet(
+                    tweet_text,
+                    cli_args["twitter_api_key"],
+                    cli_args["twitter_api_secret"],
+                    cli_args["twitter_access_token"],
+                    cli_args["twitter_access_token_secret"],
+                )
+            except Exception as e:
+                print_indented(str(e))
+                if i < MAX_ATTEMPTS:
+                    continue
+                raise e
+            else:
+                break
+    except Exception:
+        print("😿 Tweet could not be created:\n")
+        print_indented(traceback.format_exc())
+        sys.exit(1)
+    else:
+        success_msg = "🐦 Tweeted successfully!"
+        if response and cli_args["twitter_account_name"] is not None:
+            success_msg = f"{success_msg} https://twitter.com/{cli_args['twitter_account_name']}/status/{response[0]['id']}"  # noqa: E501
+        print(success_msg)
+
+
+if __name__ == "__main__":
+    main()
